f7ba568bd20bc2a3dc44e857e98014f72823466e
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-12 23:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   
- var price = Math.floor(Math.random() * 100);
+ let price = Math.floor(Math.random() * 10) + 1;
  cart.push({[item] : price});
  console.log(`${item} has been added to your cart.`);
  return cart;
