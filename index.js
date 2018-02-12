ff4370e95a689725f7828e93f625a9dd7d9f7026
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-12 23:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber === undefined){
-    return 
+    return "Sorry, we don't have a credit card on file for you.";
+  }else {
+    
   }
 }
